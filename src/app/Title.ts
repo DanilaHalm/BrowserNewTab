b557ef4120b01a0f56e08e58c6adc88b14567c4e
unknown <DanilaHalm>
2023-11-30 23:36:52 +0300

    init: initial commit

---
+++ src/app/Title.ts
@@ -0,0 +1,9 @@
+"use client";
+import styled from "styled-components";
+
+const Title = styled.h1`
+  color: red;
+  text-align: center;
+`;
+
+export default Title;
---
+++ src/app/page.tsx
@@ -0,0 +1,8 @@
+import Title from "./Title";
+export default function Home() {
+  return (
+    <main>
+      <Title> Hello </Title>
+    </main>
+  );
+}
