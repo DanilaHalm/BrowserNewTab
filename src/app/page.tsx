13a6e88f6d3c5b2184711ef8d59c895c163147c4
DanilaHalm <95771780+DanilaHalm@users.noreply.github.com>
2023-12-01 16:57:14 +0300

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,13 @@
 import Title from "./Title";
-import Input from "./components/googleSearch/input/input";
-import Form from "./components/googleSearch/form/form";
+import GoogleSearch from "./components/googleSearch/googleSearch";
+
+
 export default function Home() {
   return (
     <main>
       
-      <Title> Hello </Title>
-      <Form action="https://google.com/search" method="get" target="_blank">
-        <Input name="q" type="search" placeholder="Search on Google..."/>
-      </Form>
+      
+      <GoogleSearch />
       </main>
   );
 }
