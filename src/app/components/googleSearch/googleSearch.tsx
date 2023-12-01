1be34c6871be52d82db4529625998ebe22cf0a7c
DanilaHalm <95771780+DanilaHalm@users.noreply.github.com>
2023-12-01 19:01:29 +0300

    Update googleSearch.tsx

--- src/app/components/googleSearch/googleSearch.tsx
+++ src/app/components/googleSearch/googleSearch.tsx
@@ -1,12 +1,18 @@
+import styled from "styled-components";
 import Form from "./form/form";
 import Input from "./input/input";
 
+const GoogleSearchWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+`
+
 const GoogleSearch = () => {
-  return (<>
+  return (<GoogleSearchWrapper>
     <Form action="https://google.com/search" method="get" target="_blank">
         <Input name="q" type="search" placeholder="Search on Google..."/>
       </Form>
-  </>)
+  </GoogleSearchWrapper>)
 }
 
 export default GoogleSearch
