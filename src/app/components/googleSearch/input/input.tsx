ae3654a22c36a4171e540fafe90fd12af7135ad5
DanilaHalm <95771780+DanilaHalm@users.noreply.github.com>
2023-12-01 18:55:30 +0300

    Update input.tsx

--- src/app/components/googleSearch/input/input.tsx
+++ src/app/components/googleSearch/input/input.tsx
@@ -2,7 +2,10 @@
 import styled from "styled-components";
 
 const Input = styled.input`
-
+  border-radius: 35px;
+  background: #fffff;
+  box-shadow: inset 8px 8px 16px #bfbfbf,
+  inset -8px -8px 16px #ffffff;
 `
 
 export default Input
