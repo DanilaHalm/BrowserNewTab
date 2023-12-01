abb95f69df66ab9163fb5a05dd9e567846fa8709
DanilaHalm <95771780+DanilaHalm@users.noreply.github.com>
2023-12-01 19:23:34 +0300

    Update input.tsx

--- src/app/components/googleSearch/input/input.tsx
+++ src/app/components/googleSearch/input/input.tsx
@@ -2,9 +2,10 @@
 import styled from "styled-components";
 
 const Input = styled.input`
-  width: clamp(300px, 50vw, 1000px)
+  width: clamp(500px, 90vw, 1000px)
+  height: 3rem;
   border: none;
-  border-radius: 2rem;
+  border-radius: 25%;
   background: #fffff;
   box-shadow: inset 8px 8px 16px #bfbfbf,
   inset -8px -8px 16px #ffffff;
