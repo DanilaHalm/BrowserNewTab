268829bec2de173ba0ce177c0a8fb1c6e49bc9d8
DanilaHalm <95771780+DanilaHalm@users.noreply.github.com>
2023-12-02 11:37:15 +0300

    Update googleSearch.tsx

--- src/app/components/googleSearch/googleSearch.tsx
+++ src/app/components/googleSearch/googleSearch.tsx
@@ -8,11 +8,20 @@ const GoogleSearch = () => {
   const defineAction = async (formData: FormData) => {
     "use server"
     const query = formData.get("q")
-    redirect(`https://${query}`)
+    try {
+      const response = await fetch(`https://${query}`)
+      if(response.ok) redirect(`https://${query}`)
+      else {
+        return false
+      }
+    }
+    catch(error) {
+      console.error(error)
+    }
   }
   return (
     <Wrapper>
-      <Form action={defineAction} method="get" target="_blank">
+      <Form action={defineAction ? _ : "https://google.com/search"} method="get" target="_blank">
         <Input name="q" type="search" placeholder="Search on Google..."/>
       </Form>
     </Wrapper>
