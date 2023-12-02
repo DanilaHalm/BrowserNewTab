7424d9a38f63a3bec42c444cc48994ada009caff
DanilaHalm <95771780+DanilaHalm@users.noreply.github.com>
2023-12-02 16:27:22 +0300

    Update googleSearch.tsx

--- src/app/components/googleSearch/googleSearch.tsx
+++ src/app/components/googleSearch/googleSearch.tsx
@@ -2,13 +2,13 @@
 import Form from "./form/form";
 import Input from "./input/input";
 import Wrapper from "./wrapper/wrapper";
-import { defineAction } from "@/app/actions";
+import { defineAction, searchOnGoogle } from "@/app/actions";
 
 const GoogleSearch = () => {
 
   return (
     <Wrapper>
-      <Form action={defineAction} method="get" target="_blank">
+      <Form action={searchOnGoogle} method="get" target="_blank">
         <Input name="q" type="search" placeholder="Search on Google..."/>
       </Form>
     </Wrapper>
