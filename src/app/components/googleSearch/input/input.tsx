b5ec490b45be793379cd6ceb39059e7d8a61244b
DanilaHalm <95771780+DanilaHalm@users.noreply.github.com>
2023-12-02 08:05:31 +0300

    Update input.tsx

--- src/app/components/googleSearch/input/input.tsx
+++ src/app/components/googleSearch/input/input.tsx
@@ -5,11 +5,17 @@ const Input = styled.input`
   width: clamp(300px, 90vw, 800px);
   height: 2rem;
   padding: 0 0 0 10px;
+  color: wheat;
   border: none;
   border-radius: 8px;
   background: #2d4754;
   box-shadow: inset 20px -20px 60px #263c47,
               inset -20px 20px 60px #345261;
+
+  &:focus {
+    border: none;
+    color: white;
+  }
 `
 
 export default Input
