0b7f6883ddf95101568c4a1d02fea9acd8878489
DanilaHalm <95771780+DanilaHalm@users.noreply.github.com>
2023-12-06 01:19:20 +0300

    Update googleSearch.tsx

--- src/app/components/googleSearch/googleSearch.tsx
+++ src/app/components/googleSearch/googleSearch.tsx
@@ -2,14 +2,16 @@
 import Form from "./form/form";
 import Input from "./input/input";
 import Wrapper from "./wrapper/wrapper";
+import { useState } from "react";
 import { defineAction, searchOnGoogle } from "@/app/actions";
 
 const GoogleSearch = () => {
-
+  const [inputValue, setInputValue] = useState("");
+  
   return (
     <Wrapper>
       <Form action={searchOnGoogle} method="get" target="_blank">
-        <Input name="q" type="search" placeholder="Search on Google..."/>
+        <Input setInputValue={setInputValue} name="q" type="search" placeholder={ inputValue? inputValue : "Search on Google..."}/>
       </Form>
     </Wrapper>
   )
