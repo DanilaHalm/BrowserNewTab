f2ae9ce3e5639f23c8650f89c32079ed995b5e1d
DanilaHalm <95771780+DanilaHalm@users.noreply.github.com>
2023-12-06 21:38:39 +0300

    Update input.tsx

--- src/app/components/googleSearch/input/input.tsx
+++ src/app/components/googleSearch/input/input.tsx
@@ -44,7 +44,7 @@ const StyledInput = styled.input`
 const Input = ({ onChange, value, placeholder} : IInputProps) => {
   
   return (
-    <StyledInput onChange={(event) => onChange()} placeholder={placeholder} value={value}/>
+    <StyledInput onChange={(event) => onChange(event)} placeholder={placeholder} value={value}/>
   )
 }
 export default Input
