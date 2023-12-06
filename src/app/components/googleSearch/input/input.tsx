05df909d15a54e9db76e68e84fd70927e90dc5c5
DanilaHalm <95771780+DanilaHalm@users.noreply.github.com>
2023-12-06 08:11:54 +0300

    Update input.tsx

--- src/app/components/googleSearch/input/input.tsx
+++ src/app/components/googleSearch/input/input.tsx
@@ -38,7 +38,7 @@ const StyledInput = styled.input`
     appearance: none;
   }
 `
-const Input: React.FC<IInputProps> = (props: IInputProps) => {
+const Input: React.FC = (props: IInputProps) => {
   return (
     <StyledInput onChange={setInputValue} placeholder={placeholder} />
   )
