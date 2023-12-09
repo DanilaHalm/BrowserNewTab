ac9f739ae7e8b56cb0000bd4dc0722790e9eef1e
DanilaHalm <95771780+DanilaHalm@users.noreply.github.com>
2023-12-09 10:58:05 +0300

    Update wrapper.tsx

--- src/app/components/googleSearch/wrapper/wrapper.tsx
+++ src/app/components/googleSearch/wrapper/wrapper.tsx
@@ -1,9 +1,21 @@
 "use client"
 import styled from "styled-components";
 
-const Wrapper = styled.div`
+const StyledWrapper = styled.div`
   display: flex;
   justify-content: center;
 `
+
+interface IWrapper {
+  children?: ReactNode
+}
+
+const Wrapper = (props: IWrapper) => {
+  return (
+    <StyledWrapper>
+      {children}
+    </StyledWrapper>
+  )
+}
 
 export default Wrapper
