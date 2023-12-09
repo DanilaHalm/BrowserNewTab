e09bce8d1a46836b6476206aa6a2b52f86d30dc2
DanilaHalm <95771780+DanilaHalm@users.noreply.github.com>
2023-12-09 15:07:32 +0300

    Update googleSearch.tsx

--- src/app/components/googleSearch/googleSearch.tsx
+++ src/app/components/googleSearch/googleSearch.tsx
@@ -34,7 +34,7 @@ const GoogleSearch = () => {
         
       } catch(err) {
         console.error("false")
-        setText(JSON.stringify(err))
+        setText(`err: ${err}`)
       }
       
     } 
