3c441947dba61125eb77bc163ea448ba56787c5a
DanilaHalm <95771780+DanilaHalm@users.noreply.github.com>
2023-12-17 16:10:00 +0300

    Update input.tsx

--- src/app/components/googleSearch/input/input.tsx
+++ src/app/components/googleSearch/input/input.tsx
@@ -80,6 +80,6 @@ const StyledInput = styled.input`
   }
 `;
 const Input = () => {
-  return <StyledInput name="q" placeholder="Type URL or search on Google..." autofill={false} />;
+  return <StyledInput name="q" placeholder="Type URL or search on Google..." autoComplete="off" />;
 };
 export default Input;
