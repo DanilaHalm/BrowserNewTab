7881b388f2984e54b91126de779cbcad942d3219
DanilaHalm <95771780+DanilaHalm@users.noreply.github.com>
2023-12-17 17:22:37 +0300

    Update googleSearch.tsx

--- src/app/components/googleSearch/googleSearch.tsx
+++ src/app/components/googleSearch/googleSearch.tsx
@@ -3,9 +3,16 @@ import Form from "./form/form";
 import Input from "./input/input";
 import Wrapper from "./wrapper/wrapper";
 import { redirect } from "next/navigation";
-//import { useFormState } from "react-dom";
+import { useFormStatus } from "react-dom";
 
 import { defineAction } from "@/app/actions";
+
+const Button = () => {
+  const { pending } = useFormStatus()
+  return <button disabled={pending}>{pending ? "nice" : "test"}</button>
+}
+
+
 
 const GoogleSearch = () => {
   //const initialState = {
@@ -18,6 +25,7 @@ const GoogleSearch = () => {
     <Wrapper>
       <Form action={defineAction}>
         <Input />
+        <Button />
       </Form>
     </Wrapper>
   );
