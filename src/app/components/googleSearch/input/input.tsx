1db5f164fcc84e02e70dfd707f389ef6439f536b
DanilaHalm <95771780+DanilaHalm@users.noreply.github.com>
2023-12-17 14:06:21 +0300

    Update input.tsx

--- src/app/components/googleSearch/input/input.tsx
+++ src/app/components/googleSearch/input/input.tsx
@@ -50,19 +50,19 @@ const StyledInput = styled.input`
   &:-webkit-autofill {
     color:white;
     -webkit-text-fill-color: white;
-    box-shadow: inset 20px -20px 60px #263c47, inset -20px 20px 60px #345261;
-    background: #2d4754;
+    -webkit-box-shadow: inset 20px -20px 60px #263c47, inset -20px 20px 60px #345261;
+    -webkit-background: #2d4754;
     
     &:focus {
       -webkit-text-fill-color: white;
-      box-shadow: inset 20px -20px 60px #263c47, inset -20px 20px 60px #345261;
-      background: #2d4754;
+      -webkit-box-shadow: inset 20px -20px 60px #263c47, inset -20px 20px 60px #345261;
+      -webkit-background: #2d4754;
     }
 
     &:hover {
       -webkit-text-fill-color: white;
-      box-shadow: inset 20px -20px 60px #263c47, inset -20px 20px 60px #345261;
-      background: #2d4754;
+      -webkit-box-shadow: inset 20px -20px 60px #263c47, inset -20px 20px 60px #345261;
+      -webkit-background: #2d4754;
     }
   }
   
