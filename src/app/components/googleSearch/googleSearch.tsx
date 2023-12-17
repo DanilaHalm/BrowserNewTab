1defb11ed771f89d647c7b68bf22359a5359f8cf
DanilaHalm <95771780+DanilaHalm@users.noreply.github.com>
2023-12-17 09:36:34 +0300

    Update googleSearch.tsx

--- src/app/components/googleSearch/googleSearch.tsx
+++ src/app/components/googleSearch/googleSearch.tsx
@@ -2,22 +2,22 @@
 import Form from "./form/form";
 import Input from "./input/input";
 import Wrapper from "./wrapper/wrapper";
-import { redirect, RedirectType } from "next/navigation";
-import { useFormState } from "react-dom";
+import { redirect } from "next/navigation";
+//import { useFormState } from "react-dom";
 
 import { defineAction } from "@/app/actions";
 
 const GoogleSearch = () => {
-  const initialState = {
-    value: "",
-    link: ""
-  };
-  const [state, formAction] = useFormState(defineAction, initialState);
-  if(state?.link) redirect(state?.link, RedirectType.push)
+  //const initialState = {
+   // value: "",
+  //  link: ""
+  //};
+  //const [state, formAction] = useFormState(defineAction, initialState);
+ // if(state?.link) redirect(state?.link, RedirectType.push)
   return (
     <Wrapper>
-      <Form action={formAction}>
-        <Input dfV={state?.value} />
+      <Form action={defineAction}>
+        <Input />
       </Form>
     </Wrapper>
   );
