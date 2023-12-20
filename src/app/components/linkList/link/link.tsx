eee9606d05bb66b995ac80d928626842d4543742
DanilaHalm <95771780+DanilaHalm@users.noreply.github.com>
2023-12-21 00:17:00 +0300

    Create link.tsx

---
+++ src/app/components/linkList/link/link.tsx
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+import Link as NextLink from "next/link";
+
+const StyledLink = styled.a`
+  background: red;
+`
+
+const Link = () => {
+  
+  
+  return (<NextLink prefetch href="#" passHref>
+           <StyledLink>asdfg</StyledLink>
+         </NextLink>)
+}
+
+export default Link
