9f43685f7b7b3355531bccbd4fa9be8e91d365ce
DanilaHalm <95771780+DanilaHalm@users.noreply.github.com>
2023-12-21 22:38:22 +0300

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import GoogleSearch from "./components/googleSearch/googleSearch";
+import LinkList from "./components/linkList/linkList";
 
 export default function Home() {
   return (
     <main>
       <GoogleSearch />
+      <LinkList />
     </main>
   );
 }
