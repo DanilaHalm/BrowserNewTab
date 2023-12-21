7588ab8cc9d0f19f859f50bbb6dbe0ecadedd5f7
DanilaHalm <95771780+DanilaHalm@users.noreply.github.com>
2023-12-21 23:43:41 +0300

    Update link.tsx

--- src/app/components/linkList/link/link.tsx
+++ src/app/components/linkList/link/link.tsx
@@ -8,10 +8,12 @@ interface ILinkItem {
 }
 
 const StyledLink = styled(Link)`
+  margin: 0;
   display: flex;
   justify-content: center;
   align-items: center;
   color: white;
+  text-decoration: none;
   font-size: 1rem;
   height: 3rem;
   transition: 0.5s;
@@ -19,6 +21,10 @@ const StyledLink = styled(Link)`
   border-radius: 8px;
   background: linear-gradient(145deg, #304c5a, #29404c);
   box-shadow: 5px 5px 10px #142026, -5px -5px 10px #466e82;
+
+  &:hover {
+    box-shadow: 2px 2px 5px #7296a8, -2px 2px 5px #7296a8, -2px -2px 5px #7296a8, 2px -2px 5px #7296a8;
+  }
 `
 
 const LinkItem = ({href,name}:ILinkItem) => {
