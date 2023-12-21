46bab1a4be9353414d904fd588a2421bb207bb4e
DanilaHalm <95771780+DanilaHalm@users.noreply.github.com>
2023-12-21 22:29:28 +0300

    Update link.tsx

--- src/app/components/linkList/link/link.tsx
+++ src/app/components/linkList/link/link.tsx
@@ -2,15 +2,20 @@
 import styled from "styled-components";
 import Link from "next/link";
 
+interface ILinkItem {
+  href: string
+  name: string
+}
+
 const StyledLink = styled.a`
   background: red;
 `
 
-const LinkItem = () => {
+const LinkItem = ({href,name}:ILinkItem) => {
   
   
-  return (<Link prefetch href="#" passHref>
-           <StyledLink>asdfg</StyledLink>
+  return (<Link prefetch href={href} passHref>
+           <StyledLink>{name}</StyledLink>
          </Link>)
 }
 
