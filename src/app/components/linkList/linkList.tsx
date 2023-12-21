a1c406ca29970ed924d1247c2977cbc41dcb9a2d
DanilaHalm <95771780+DanilaHalm@users.noreply.github.com>
2023-12-21 22:36:36 +0300

    Update linkList.tsx

--- src/app/components/linkList/linkList.tsx
+++ src/app/components/linkList/linkList.tsx
@@ -1,8 +1,33 @@
 import Wrapper from "./wrapper/wrapper";
 import LinkItem from "./link/link";
 
+const links = [
+  {
+    href: "https://github.com",
+    name: "GitHub"
+  },
+    {
+    href: "https://youtube.com",
+    name: "YouTube"
+  },
+    {
+    href: "https://vk.ru",
+    name: "Vkontakte"
+  },
+    {
+    href: "https://vercel.com",
+    name: "Vercel"
+  },
+    {
+    href: "https://nextjs.org",
+    name: "NextJS"
+  },
+]
+
 const LinkList = () => {
-  return 
+  return links?.map(link => {
+    <LinkItem href={link?.href} name={link?.name}/>
+  })
 }
 
 export default LinkList
