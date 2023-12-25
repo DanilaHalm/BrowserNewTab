edd20dd4d997a3750ea2f90cd8305a385a7ddfa8
DanilaHalm <95771780+DanilaHalm@users.noreply.github.com>
2023-12-25 15:02:44 +0300

    Update settingsButton.tsx

--- src/app/components/appWrapper/settingsButton.tsx
+++ src/app/components/appWrapper/settingsButton.tsx
@@ -3,10 +3,14 @@
 import styled from "styled-components";
 
 const StyledButton = styled.button`
-  width: 20px;
-  height: 20px;
+  width: 3rem;
+  height: 3rem;
   border: none;
   border-radius: 8px;
+  justify-self: end;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 `
 
 const SettingsButton = () => {
