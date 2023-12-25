5ef90b65faac025328f525dc438623ea4769e134
DanilaHalm <95771780+DanilaHalm@users.noreply.github.com>
2023-12-25 11:58:39 +0300

    Create settingsButton.tsx

---
+++ src/app/components/appWrapper/settingsButton.tsx
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+const StyledButton = styled.button`
+  width: 20px;
+  height: 20px;
+  border: none;
+  border-radius: 8px;
+`
+
+const SettingButton = () => {
+  return (
+    <StyledButton>{"Ss"}</StyledButton>
+  )
+}
+
+export default SettingButton
