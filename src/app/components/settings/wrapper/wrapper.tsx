2f5b41f6c0aa2dae9e6e861094401756b15a829b
DanilaHalm <95771780+DanilaHalm@users.noreply.github.com>
2023-12-25 17:48:17 +0300

    Update wrapper.tsx

--- src/app/components/settings/wrapper/wrapper.tsx
+++ src/app/components/settings/wrapper/wrapper.tsx
@@ -5,6 +5,7 @@ import { PropsWithChildren } from 'react';
 const StyledWrapper = styled.div`
   width: clamp(300px, 100vw, 800px);
   height: 100vh;
+  right: 50vw;
   position: absolute;
   z-index: 5;
   background: red;
