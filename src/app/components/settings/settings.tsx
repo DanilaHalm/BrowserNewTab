5db59e9de5f6bb3096994b6c4c2ff7c9b2baf9ad
DanilaHalm <95771780+DanilaHalm@users.noreply.github.com>
2023-12-25 17:56:34 +0300

    Update settings.tsx

--- src/app/components/settings/settings.tsx
+++ src/app/components/settings/settings.tsx
@@ -1,12 +1,15 @@
 "use client"
 import Wrapper from "./wrapper/wrapper";
 import SettingsButton from "../appWrapper/settingsButton";
+import { useState } from "react";
 
 const Settings = () => {
+  const [isActive, setIsActive] = useState(false)
+  
   return (
     <>
-      <SettingsButton />
-      <Wrapper>
+      <SettingsButton setIsActive={setIsActive}/>
+      <Wrapper isActive={isActive}>
       </Wrapper>
     </>
   )
