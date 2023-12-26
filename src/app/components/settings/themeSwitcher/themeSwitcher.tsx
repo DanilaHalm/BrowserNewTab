b7004dfc384d6c1cf02e1471daf838d5bf1d33d1
DanilaHalm <95771780+DanilaHalm@users.noreply.github.com>
2023-12-26 12:28:12 +0300

    Update themeSwitcher.tsx

--- src/app/components/settings/themeSwitcher/themeSwitcher.tsx
+++ src/app/components/settings/themeSwitcher/themeSwitcher.tsx
@@ -1,9 +1,12 @@
 import styled from "styled-components";
 
 const StyledThemeSwitcher = styled.div`
+  margin: 1rem auto;
+  width: 90%;
   height: 5rem;
   color: ${props => props.theme.color};
   border: 1px ${props => props.theme.color} solid;
+  border-radius: 8px;
   display: flex;
   justify-content: space-around;
 `
