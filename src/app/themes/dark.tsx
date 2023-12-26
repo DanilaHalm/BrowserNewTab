2ca98828b56aeb5611ab0adb4148bbb3c93eb6a7
DanilaHalm <95771780+DanilaHalm@users.noreply.github.com>
2023-12-26 09:01:20 +0300

    Update dark.tsx

--- src/app/themes/dark.tsx
+++ src/app/themes/dark.tsx
@@ -1,14 +1,28 @@
-const darkTheme = {
+interface IDark {
+  name: "string",
+  color: "string",
+  background: "string",
+  boxshadow: "string",
+  focus: {
+    background: "string",
+    boxshadow: "string",
+  },
+  hover: {
+    boxshadow: "string"
+  }
+}
+
+const darkTheme: IDark = {
   name: "Dark",
   color: "white",
   background: "linear-gradient(145deg, #111829, #0e1422)",
-  box-shadow:  "5px 5px 10px #06090f, -5px -5px 10px #1a233d",
+  boxshadow:  "5px 5px 10px #06090f, -5px -5px 10px #1a233d",
   focus: {
     background: "#101626",
-    box-shadow: "inset 5px 5px 10px #06090f, inset -5px -5px 10px #1a233d"
+    boxshadow: "inset 5px 5px 10px #06090f, inset -5px -5px 10px #1a233d"
   },
   hover: {
-    box-shadow: "2px 2px 5px #7296a8, -2px 2px 5px #7296a8, -2px -2px 5px #7296a8, 2px -2px 5px #7296a8"
+    boxshadow: "2px 2px 5px #7296a8, -2px 2px 5px #7296a8, -2px -2px 5px #7296a8, 2px -2px 5px #7296a8"
   }
 }
 
