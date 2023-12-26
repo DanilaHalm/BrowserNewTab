66031761150bdbce9365a04b4a61db01b5c3ccd1
DanilaHalm <95771780+DanilaHalm@users.noreply.github.com>
2023-12-26 08:44:07 +0300

    Create dark.tsx

---
+++ src/app/themes/dark.tsx
@@ -0,0 +1,15 @@
+const darkTheme = {
+  name: "Dark",
+  color: "white",
+  background: "linear-gradient(145deg, #111829, #0e1422)",
+  box-shadow:  "5px 5px 10px #06090f, -5px -5px 10px #1a233d",
+  focus: {
+    background: "#101626",
+    box-shadow: "inset 5px 5px 10px #06090f, inset -5px -5px 10px #1a233d"
+  },
+  hover: {
+    box-shadow: "2px 2px 5px #7296a8, -2px 2px 5px #7296a8, -2px -2px 5px #7296a8, 2px -2px 5px #7296a8"
+  }
+}
+
+export default darkTheme
