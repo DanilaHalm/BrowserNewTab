9851f5e55ad370e29543e75a9dfb2abc9811a673
DanilaHalm <95771780+DanilaHalm@users.noreply.github.com>
2023-12-26 12:53:24 +0300

    Create light.tsx

---
+++ src/app/themes/light.tsx
@@ -0,0 +1,19 @@
+import ITheme from "./interface";
+
+const lightTheme: ITheme = {
+  name: "Light",
+  color: "white",
+  background: "linear-gradient(145deg, #111829, #0e1422)",
+  boxshadow:  "5px 5px 10px #06090f, -5px -5px 10px #1a233d",
+  focus: {
+    background: "#101626",
+    boxshadow: "inset 5px 5px 10px #06090f, inset -5px -5px 10px #1a233d"
+  },
+  hover: {
+    boxshadow: "2px 2px 5px #7296a8, -2px 2px 5px #7296a8, -2px -2px 5px #7296a8, 2px -2px 5px #7296a8"
+  },
+  settingsImage: "/settings-white.png",
+  settingsXmarkImage: "/xmark-white.png"
+}
+
+export default lightTheme
