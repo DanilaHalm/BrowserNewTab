d20b4e17ba83336092e447418dadd897e24bc97d
DanilaHalm <95771780+DanilaHalm@users.noreply.github.com>
2023-12-26 13:12:42 +0300

    Update light.tsx

--- src/app/themes/light.tsx
+++ src/app/themes/light.tsx
@@ -4,10 +4,10 @@ const lightTheme: ITheme = {
   name: "Light",
   color: "#101626",
   background: "linear-gradient(145deg, #ebe2f0, #c6beca)",
-  boxshadow:  "5px 5px 10px #58545a, -5px -5px 10px #ffffff",
+  boxshadow:  "5px 5px 10px #9a949d, -5px -5px 10px #ffffff",
   focus: {
     background: "#dcd3e0",
-    boxshadow: "inset 5px 5px 10px #58545a, inset -5px -5px 10px #ffffff"
+    boxshadow: "inset 5px 5px 10px #9a949d, inset -5px -5px 10px #ffffff"
   },
   hover: {
     boxshadow: "2px 2px 5px #7296a8, -2px 2px 5px #7296a8, -2px -2px 5px #7296a8, 2px -2px 5px #7296a8"
