e5a7e3cf09bbf4d01dc31110806cf7355aa4a418
DanilaHalm <95771780+DanilaHalm@users.noreply.github.com>
2023-12-26 12:43:18 +0300

    Update themeSwitcherButton.tsx

--- src/app/components/settings/themeSwitcherButton/themeSwitcherButton.tsx
+++ src/app/components/settings/themeSwitcherButton/themeSwitcherButton.tsx
@@ -2,9 +2,11 @@ import styled from "styled-components";
 
 const StyledButton = styled.button`
   width: 8rem;
+  height: 3rem;
   border: 1px ${props => props.theme.color} solid;
   border-radius: 8px;
   color: ${props => props.theme.color};
+  background: ${props => props.theme.background};
   display: flex;
   justify-content: center;
   align-items: center;
