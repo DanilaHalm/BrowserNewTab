7f557650f8b9fa20a1b5012bc17b272193ba3bd0
DanilaHalm <95771780+DanilaHalm@users.noreply.github.com>
2023-12-26 11:23:58 +0300

    Update settingsButton.tsx

--- src/app/components/appWrapper/settingsButton.tsx
+++ src/app/components/appWrapper/settingsButton.tsx
@@ -33,8 +33,8 @@ const SettingsButton = ({ isActive, setIsActive } : ISettingsButton) => {
   const [settingsImage,setSettingsImage] = useState(theme.settingsImage)
   
   const handleSettings = () => {
+    setSettingsImage(isActive ? theme.settingsImage : theme.settingsXmarkImage)
     setIsActive(!isActive)
-    setSettingsImage(isActive ? theme.settingsXmarkImage : theme.settingsImage)
   }
   
   return (
