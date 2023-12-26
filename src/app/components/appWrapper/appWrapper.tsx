c1b32c40332e59c6395b566b68d0f321158e18b6
DanilaHalm <95771780+DanilaHalm@users.noreply.github.com>
2023-12-26 09:10:25 +0300

    Update appWrapper.tsx

--- src/app/components/appWrapper/appWrapper.tsx
+++ src/app/components/appWrapper/appWrapper.tsx
@@ -3,10 +3,11 @@ import GoogleSearch from "../googleSearch/googleSearch";
 import LinkList from "../linkList/linkList";
 import Wrapper from "./wrapper/wrapper";
 import { ThemeProvider } from "styled-components";
+import darkTheme from "../../themes/dark";
 
 const AppWrapper = () => {
   return (
-    <ThemeProvider theme={{color:"red"}}>
+    <ThemeProvider theme={darkTheme}>
       <Wrapper>
         <GoogleSearch />
         <LinkList />
