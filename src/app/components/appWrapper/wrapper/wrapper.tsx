abf7845d058e24e94abbaf8e7ff4aacbf300dede
DanilaHalm <95771780+DanilaHalm@users.noreply.github.com>
2023-12-26 08:17:59 +0300

    Create wrapper.tsx

---
+++ src/app/components/appWrapper/wrapper/wrapper.tsx
@@ -0,0 +1,24 @@
+"use client"
+import styled from "styled-components";
+import { PropsWithChildren } from 'react';
+import Settings from "../../settings/settings";
+
+const StyledWrapper = styled.div`
+  margin: 0;
+  padding: 0;
+  height: 100vh;
+  transition: 0.5s;
+  position: relative;
+  overflow: hidden;
+`
+
+const Wrapper = ({children} : PropsWithChildren) => {
+  return (
+    <StyledWrapper>
+      <Settings />
+      {children}
+    </StyledWrapper>
+  )
+}
+
+export default Wrapper
